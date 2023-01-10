c4ce7d6d15d879b8ec50d159394ffbb4e98b2f9d
hidden-programmer <96066541+hidden-programmer@users.noreply.github.com>
2023-01-10 16:22:29 +0530

    Create gross-sallary.py

---
+++ gross-sallary.py
@@ -0,0 +1,10 @@
+x = input("Enter Your Salary")
+y= int(x)*int(40)/int(100)
+
+print("Dearness allowance  ",y)
+z=int(x)*int(20)/int(100)
+
+print("House Rent Allowance",z)
+r=int(x)+int(y)+int(z)
+print("Gross salary ",r)
+

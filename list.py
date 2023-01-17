1c47718f8af5543842c385451e1ef153e5ba9373
hidden-programmer <96066541+hidden-programmer@users.noreply.github.com>
2023-01-17 16:21:14 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,24 @@
+names = ['Anil', 'Amol', 'Aditya', 'Avi', 'Alka']
+print(names)
+
+names.insert(2,'Anuj')
+print(names)
+names.append('zulu')
+print(names)
+
+
+names.remove('Avi')
+print(names)
+
+names[0] = 'AnilKumar'
+print(names)
+
+
+
+names.sort( )
+print(names)
+
+
+names.reverse( )
+print(names)
+
